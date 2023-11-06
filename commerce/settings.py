3631dc1483942800152668119a11a362df89bab0
mhodsaifansari <mhodsaif.sps@gmail.com>
2023-11-06 17:19:17 +0530

    edits new

--- commerce/settings.py
+++ commerce/settings.py
@@ -70,7 +70,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 
 ]
-CORS_ALLOWED_ORIGINS = os.getenv("CORS_ALLOWED_ORIGINS", "*").split(",")
+# CORS_ALLOWED_ORIGINS = os.getenv("CORS_ALLOWED_ORIGINS", "*").split(",")
 CORS_ORIGIN_ALLOW_ALL=True
 CORS_ALLOW_CREDENTIALS=True
 
